4270abf13e5d04f7ea04c3fbddd2f90090832dd9
Shao <ericqyhan@gmail.com>
2019-05-28 20:10:54 -0400

    First JAVA Homework

---
+++ 20190529/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class FormattedOutput {
+    public static void main(String[] args) {
+       // System.out.format("The value of the float is %f, while the value of the integer is %d, and the string is %s", 4.23, 5, "cat");
+        System.out.format("My name is %s and I am %d years old.", "Eric Han", 28);
+
+    }
+}
---
+++ 20190529/U1M1L1Katas/src/main/java/com/company/HiNeighbor.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class HiNeighbor {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi Neighbor!");
+        System.out.println("My name is Eric Han.");
+        System.out.println("I'm from East Hanover.");
+        System.out.println("I have 1 siblings.");
+        System.out.println("Red is my favorite color.");
+        System.out.println("I listen to Lo-Fi music.");
+        System.out.println("My favorite hobby is MMA.");
+        System.out.println("Nice to meet you!");
+        System.out.println("Good bye!!!");
+    }
+}
