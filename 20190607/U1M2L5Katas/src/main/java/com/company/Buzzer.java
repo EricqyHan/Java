994366531729cf23812feb447058fb51815b0117
Shao <ericqyhan@gmail.com>
2019-06-06 12:03:55 -0400

    First push

---
+++ 20190607/U1M2L5Katas/src/main/java/com/company/Buzzer.java
@@ -0,0 +1,17 @@
+package com.company;
+import com.company.interfaces.Alarm;
+import com.company.interfaces.Clock;
+
+public class Buzzer implements Alarm, Clock {
+    public void sound(){
+
+    };
+    public void displayTime(){
+
+    };
+    public void timer(int hour, int minute){
+
+    };
+}
+
+
---
+++ 20190607/U1M2L5Katas/src/main/java/com/company/Triangle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.interfaces.Shape;
+
+public class Triangle implements Shape {
+    public double perimeter(){
+        return 0;
+    };
+    public double area(){
+        return 0;
+    };
+}
