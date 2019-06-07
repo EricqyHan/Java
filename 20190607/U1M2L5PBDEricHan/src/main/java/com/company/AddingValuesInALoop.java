c34b82b305b21c0e9ea2e55738d3ce94174c2c0c
Shao <ericqyhan@gmail.com>
2019-06-06 23:46:48 -0400

    last push

--- 20190607/U1M2L5PBDEricHan/src/main/java/com/company/AddingValuesInALoop.java
+++ 20190607/U1M2L5PBDEricHan/src/main/java/com/company/AddingValuesInALoop.java
@@ -1,24 +1,22 @@
-//package com.company;
-//
-//import java.util.Scanner;
-//
-//public class AddingValuesInALoop {
-//    public static void main(String[] args) {
-//        Scanner newScanner = new Scanner(System.in);
-//
-//        System.out.println("I will add up the numbers you give me. ");
-//        String userInput = newScanner.nextLine();
-//
-//        int userNumber;
-//        userNumber = Integer.parseInt(userInput);
-//
-//
-//        while (userNumber != 0) {
-//            System.out.println("The total so far is " + n+n;
-//            String n = newScanner.nextLine();
-//
-//
-//        }
-//    }
-//
-//}
+package com.company;
+
+import java.util.Scanner;
+
+public class AddingValuesInALoop {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("I will add up the numbers you give me. ");
+        String userInput = myScanner.nextLine();
+        int addValues = Integer.parseInt(userInput);
+
+
+        while(addValues != 0){
+            System.out.println("Number: ");
+            userInput = myScanner.nextLine();
+            addValues = Integer.parseInt(userInput);
+        }
+        System.out.println("The total is: " + addValues);
+    }
+
+}
--- 20190607/U1M2L5PBDEricHan/src/main/java/com/company/SafeSquareRoot.java
+++ 20190607/U1M2L5PBDEricHan/src/main/java/com/company/SafeSquareRoot.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class SafeSquareRoot {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Square Root");
+        System.out.println("Enter a number: ");
+
+        String userSquare = myScanner.nextLine();
+        int square =Integer.parseInt(userSquare);
+
+
+        while(square < 0) {
+            System.out.println("You can't take the square root of a negative number, silly.");
+            System.out.println("Try again: ");
+            square = Integer.parseInt(myScanner.nextLine());
+        }
+        double x = Math.sqrt(square);
+        System.out.println("The square root of " + square + " is " + x +".");
+    }
 }
