6f581f9cda705583740c0e2b17c8d14cd50ff2b7
EricqyHan <ericqyhan@gmail.com>
2019-06-09 16:48:02 -0400

    Update ArrayFunReverseIt.java

--- U1-M1-Summative-Han-Eric/src/main/java/com/company/ArrayFunReverseIt.java
+++ U1-M1-Summative-Han-Eric/src/main/java/com/company/ArrayFunReverseIt.java
@@ -25,16 +25,3 @@ public class ArrayFunReverseIt {
         }
     }
 }
-
-//    public static int[] reverse(int[] numbers) {
-//
-//        int[] reversed = new int[numbers.length];//
-//        for(int i = 0; i < numbers.length; i++) {
-//            // length - (i + 1) is the n-th to last element
-//            // so when i = 0, it would be the last element
-//            // when i = 3, it would be the fourth to last element since i=3 is the 4th element, etc
-//            reversed[i] = numbers[ numbers.length - (i + 1) ];
-//        }
-//
-//        return reversed;
-//    }
