8f1846c4291b4d774635bdac7e5898c9d249923d
Shao <ericqyhan@gmail.com>
2019-06-09 23:45:28 -0400

    U1M2L7 Kata Completed

---
+++ 20190610/U1M2L7Katas/src/main/java/com/company/Engine.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Engine {
+public void createEngine(){
+
+}
+    private String engine;
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+
+}
