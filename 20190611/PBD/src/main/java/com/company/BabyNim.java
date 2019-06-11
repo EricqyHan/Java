226ec6e8e6786159c3caee1ea70313be0e3690f5
Shao <ericqyhan@gmail.com>
2019-06-11 08:42:49 -0400

    fixed issues

--- 20190611/PBD/src/main/java/com/company/BabyNim.java
+++ 20190611/PBD/src/main/java/com/company/BabyNim.java
@@ -14,21 +14,23 @@ public class BabyNim {
 
         do{
             System.out.println("Choose a pile: ");
+            System.out.println(" A : " + a + " B : " + b + " C : " + c);
             userinput = myScanner.nextLine();
             System.out.println("How many to remove from pile " + userinput);
             removeFrom = Integer.parseInt(myScanner.nextLine());
+            //System.out.println("A : " + a + " B : " + b + " C : " + c);
 
 
             if (userinput.equals("a")) {
                 a = a - removeFrom;
             } else if (userinput.equals("b")) {
-                b = a - removeFrom;
+                b = b - removeFrom;
             } else if (userinput.equals("c")) {
                 c = c - removeFrom;
             } else {
                 System.out.println("Pile invalid. Select another pile.");
             }
-            System.out.println(" A: " + a + " B: " + b + " C: " + c);
+            System.out.println(" A : " + a + " B : " + b + " C : " + c);
 
         } while(a >0 || b > 0 || c > 0);{
             System.out.println("All piles are empty. Good Job!");
--- 20190611/PBD/src/main/java/com/company/Nim.java
+++ 20190611/PBD/src/main/java/com/company/Nim.java
@@ -13,45 +13,50 @@ public class Nim {
         int a = 3;
         int b = 3;
         int c = 3;
+        int removeFrom;
+        String userinput;
         String player1 = userInput1;
         String player2 = userInput2;
 
+        do{
+            System.out.println(player1 + " Choose a pile: ");
+            System.out.println(" A : " + a + "| B : " + b + "| C : " + c);
+            userinput = myScanner.nextLine();
+            System.out.println("How many to remove from pile " + userinput);
+            removeFrom = Integer.parseInt(myScanner.nextLine());
 
-        do {
-            System.out.println(player1 + " choose a pile: ");
-            String userinput = myScanner.nextLine();
-            player1 = myScanner.nextLine();
+            //Player 1
             if (userinput.equals("a")) {
-                a--;
+                a = a - removeFrom;
             } else if (userinput.equals("b")) {
-                b--;
+                b = b - removeFrom;
             } else if (userinput.equals("c")) {
-                c--;
+                c = c - removeFrom;
             } else {
                 System.out.println("Pile invalid. Select another pile.");
             }
-            System.out.println(" A: " + a + " B: " + b + " C: " + c);
+            System.out.println(" A : " + a + "| B : " + b + "| C : " + c);
+            System.out.println("                                       ");
+            //Player 2
+            System.out.println(player2 + " Choose a pile: ");
+            System.out.println(" A : " + a + "| B : " + b + "| C : " + c);
+            userinput = myScanner.nextLine();
+            System.out.println("How many to remove from pile " + userinput);
+            removeFrom = Integer.parseInt(myScanner.nextLine());
 
-            System.out.println(player2 + " choose a pile: ");
-            player2 = myScanner.nextLine();
             if (userinput.equals("a")) {
-                a--;
+                a = a - removeFrom;
             } else if (userinput.equals("b")) {
-                b--;
+                b = b - removeFrom;
             } else if (userinput.equals("c")) {
-                c--;
+                c = c - removeFrom;
             } else {
                 System.out.println("Pile invalid. Select another pile.");
             }
-            System.out.println(" A: " + a + " B: " + b + " C: " + c);
+            System.out.println(" A : " + a + "| B : " + b + "| C : " + c);
 
-
-        } while (a > 0 || b > 0 || c > 0);
-        {
+        }  while((a != 0) && (b != 0) && (c != 0));{
             System.out.println("All piles are empty. Good Job!");
         }
     }
-
-//        System.out.println(userInput1);
-//        System.out.println(userInput2);
 }
